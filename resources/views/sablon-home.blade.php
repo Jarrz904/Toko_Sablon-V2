e687f55c0b4133be4131d632649be838d764fd5a
Jarrz904 <mfajarsidik64@gmail.com>
2025-12-29 23:33:37 +0700

    sablon-home

--- resources/views/sablon-home.blade.php
+++ resources/views/sablon-home.blade.php
@@ -332,7 +332,7 @@
 
                     <div>
                         <h4 class="text-amber-500 font-bold uppercase text-xs tracking-widest mb-3 italic">Facebook</h4>
-                        <a href="https://facebook.com/journastudio" target="_blank" class="text-2xl md:text-3xl font-bold text-white hover:text-[#1877F2] transition-all flex items-center gap-4 group">
+                        <a href="https://www.facebook.com/journaart" target="_blank" class="text-2xl md:text-3xl font-bold text-white hover:text-[#1877F2] transition-all flex items-center gap-4 group">
                             <div class="p-3 bg-white/5 rounded-2xl group-hover:bg-[#1877F2]/20 transition">
                                 <svg class="w-7 h-7 fill-current" viewBox="0 0 24 24"><path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/></svg>
                             </div>
